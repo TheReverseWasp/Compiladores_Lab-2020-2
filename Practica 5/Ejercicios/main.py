9afc1ec343d905c4373f6a32761bde9f92d84a8d
Ricardo Manuel Lazo Vásquez <ghop.0703@gmail.com>
2020-10-11 17:58:08 -0500

    Practica 5 Completada

---
+++ Practica 5/Ejercicios/main.py
@@ -0,0 +1,13 @@
+from TAS import *
+
+def main():
+    gram = Gram_Prac4()
+    # print(gram.getSiguiente("E"))
+    print("Primeros: ")
+    print(gram.getPrimeros())
+    print("Siguientes: ")
+    print(gram.getSiguientes())
+    
+
+if __name__ == "__main__":
+    main()
